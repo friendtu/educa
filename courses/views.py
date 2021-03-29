7cb2e93747450e5eee3826e47520de1306cde18f
friendtu <friendtu@gmail.com>
2021-03-29 22:02:17 +0800

    initial formset

--- courses/views.py
+++ courses/views.py
@@ -4,6 +4,10 @@ from django.views.generic.list import ListView
 from .models import Course
 from django.views.generic.edit import CreateView,DeleteView,UpdateView
 from django.contrib.auth.mixins import LoginRequiredMixin,PermissionRequiredMixin
+from .forms import ModuleFormSet
+from django.shortcuts import get_object_or_404,redirect
+from django.views.generic import from TemplateResponseMixin
+
 
 # Create your views here.
 class OwnerMixin(object):
@@ -32,8 +36,32 @@ class CourseCreateView(PermissionRequiredMixin,OwnerCourseEditMixin,CreateView):
 
 class CourseUpdateView(PermissionRequiredMixin,OwnerCourseEditMixin,UpdateView):
     permission_required='course.change_course'
+
+class CourseModuleUpdateView(TemplateResponseMixin,View)
+    template_name='courses/manage/module/formset.html'
+    couse=None
+
+
+    def get(self,request):
+        formset=ModuleFormSet(instance=self.course)
+        return self.render_to_response({'formset':formset,
+                                        'coure':self.coure
+                                        })
+
+    def post(self,request,*args, **kwargs):
+        formset=ModuleFormSet(request.post)
+        if formset.is_valid():
+            formset.save()
+            return redirect('manage_course_list')
+        return self.render_to_response({'course':self.course,
+                                        'formset':formset})
+            
+ 
+    def dispatch(self,request,pk):
+        self.course=get_object_or_404(Course,id=pk,Owner=request.user)
+        return super(request,pk)
+
     
 class CourseDeleteView(PermissionRequiredMixin,OwnerCourseMixin,DeleteView):
     permission_required='course.delete_course'
     template_name="courses/manage/course/delete.html"
-    success_url=reverse_lazy('manage_course_list')

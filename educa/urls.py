4bbe835120d89d3088474a9e333adbeb65a32544
friendtu <friendtu@gmail.com>
2021-04-13 15:47:53 +0800

    fix login url

--- educa/urls.py
+++ educa/urls.py
@@ -19,7 +19,7 @@ from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('accounts/login',auth_views.LoginView.as_view(),name='login'),
-    path('accounts/logout',auth_views.LogoutView.as_view(),name='logout'),
+    path('accounts/login/',auth_views.LoginView.as_view(),name='login'),
+    path('accounts/logout/',auth_views.LogoutView.as_view(),name='logout'),
     path('courses/',include('courses.urls')),
 ]
